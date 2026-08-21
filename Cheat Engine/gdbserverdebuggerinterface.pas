bc965042b4e0b68f88a1ff93f426cb087ef14436
ra1nyxin <ra1nyxin@users.noreply.github.com>
2026-08-22 02:32:07 +0800

    拦截GDB二进制接收中的无效长度

--- Cheat Engine/gdbserverdebuggerinterface.pas
+++ Cheat Engine/gdbserverdebuggerinterface.pas
@@ -634,6 +634,19 @@ begin
       begin
         if not waitForData(0) then break; //nothing left to read at the moment (might have more later)
         i:=fprecv(socket, buf,4096,0);
+        if i<=0 then
+        begin
+          if socket<>0 then
+          begin
+            {$ifdef windows}
+            closesocket(socket);
+            {$else}
+            fpclose(socket);
+            {$endif}
+            socket:=0;
+          end;
+          exit;
+        end;
 
         if binaryReceiveBufferPos+i>=binaryReceiveBufferMax then
         begin
@@ -644,8 +657,7 @@ begin
         copymemory(@binaryReceiveBuffer[binaryReceiveBufferPos], buf, i);
         inc(binaryReceiveBufferPos,i);
 
-        if i>0 then
-          receivedData:=true;
+        receivedData:=true;
         //find a buffer inside the receiveBuffer and if found, return it as a result (and ack)
       end;
 
